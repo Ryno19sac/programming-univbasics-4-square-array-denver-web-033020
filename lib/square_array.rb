28004377321de66f2795b45653870b73e832583e
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-10 04:07:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
- #new_array = []
+ new_array = []
  counter = 0 
   while array[counter] do
    puts array[counter] << array ** 2
     counter += 1
-  #puts new_array
+  puts new_array
 end
     
  
